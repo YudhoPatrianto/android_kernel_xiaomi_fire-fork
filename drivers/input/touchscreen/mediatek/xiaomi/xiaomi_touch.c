097d2eab4c095a647424de8d2d4617e365725840
YudhoPatrianto <kydh01123@gmail.com>
2025-11-06 01:14:29 +0000

    driver: input: touchscreen: remove unused variables
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- drivers/input/touchscreen/mediatek/xiaomi/xiaomi_touch.c
+++ drivers/input/touchscreen/mediatek/xiaomi/xiaomi_touch.c
@@ -180,8 +180,6 @@ int xiaomitouch_register_modedata(struct xiaomi_touch_interface *data)
 
 int update_palm_sensor_value(int value)
 {
-	struct xiaomi_touch *dev = NULL;
-
 	mutex_lock(&xiaomi_touch_dev.palm_mutex);
 
 	if (!touch_pdata) {
@@ -189,7 +187,7 @@ int update_palm_sensor_value(int value)
 		return -ENODEV;
 	}
 
-	dev = touch_pdata->device;
+	touch_pdata->device;
 
 	if (value != touch_pdata->palm_value) {
 		printk("%s value:%d\n", __func__, value);
